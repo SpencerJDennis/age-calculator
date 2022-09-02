1307059fbf8aa9a74f94b45b683bb5c9c1d7bc1c
Spencer Dennis <sjdennis2113@gmail.com>
2022-09-02 16:59:13 -0700

    add new function and testing for Mercury Age

--- __tests__/scripts.test.js
+++ __tests__/scripts.test.js
@@ -7,4 +7,9 @@ test('should determine user age depended on userInput', () => {
   let user1 = new Age(21)
   expect(user1.userInput).toEqual(21);
 });
+
+test('should return mercuryAge 5.04', () => {
+  return mercuryYear
+  expect(mercuryYear).toEqual(5.04)
+});
 });
--- src/scripts.js
+++ src/scripts.js
@@ -2,4 +2,11 @@ export default class Age {
   constructor(userInput) {
     this.userInput = userInput
   }
+
+mercuryAge = function() {
+  let user1 = new Age(userInput)
+  let mercuryYear = .24
+  let mercuryAge = user1 * mercuryYear;
+  return mercuryAge
 }
+};
